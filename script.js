16c1cb6e943ab2cdce84c4f37abb1479218884d5
christiantoves <83604594+christiantoves@users.noreply.github.com>
2021-05-21 19:01:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ for(i=1; i<=100; i++){
     if (i%3==0){
         console.log("Fizz")
     }
-    if (i%5==0){
+    else if (i%5==0){
         console.log("Buzz")
     }
-    if (i%3==0 && i%5==0){
+    
+    else if (i%3==0 && i%5==0){
         console.log("FizzBuzz")
     }
     else (console.log(i))
